bba61a964ac383eb554c02dff77709235e888ade
dorian2909 <doumanaca@est.utn.ac.cr>
2024-04-18 20:52:18 -0600

    Subida 2 xd

--- script/scCont.js
+++ script/scCont.js
@@ -1,58 +1,52 @@
 function calculoCont(){
     
 }
+var datos = ["Costa Rica", "Venezuela", "Honduras", "Salvador", "Nicaragua"];
 
-function fill(){
+function fill() {
+    
+    var fil = document.getElementById("fila");
 
-     // vect
-     var datos = ["Costa Rica", "Venezuela", "Honduras", "Salvador", "Nicaragua"];
+    for (var i = 0; i < datos.length; i++) {
+        var nuevIn = document.createElement("input");
+        nuevIn.id = "inicio_" + i; // ID único para el inicio
+        var nuevIn2 = document.createElement("input");
+        nuevIn2.id = "final_" + i; // ID único para el final
+        var nuevtd = document.createElement("td");
+        var nuevtd2 = document.createElement("td");
+        var nuevoTr = document.createElement("tr");
 
-     var fil = document.getElementById("fila");
- 
-     for (var i = 0; i < datos.length; i++) {
-         var nuevIn = document.createElement("input");
-         nuevIn.id="inicio";
-         var nuevIn2 = document.createElement("input");
-         nuevIn2.id="final";
-         var nuevtd = document.createElement("td");
-         var nuevtd2 = document.createElement("td");
-
-         var nuevoTr = document.createElement("tr");
-         var nuevoTr2 = document.createElement("tr");
-        
-         // set datito
-         nuevoTr.innerText = datos[i];
-         // Agregar el nuevo <td> 
-         fil.appendChild(nuevoTr);
-         nuevoTr.appendChild(nuevtd);
-         nuevtd.appendChild(nuevIn);
-         fil.appendChild(nuevtd2);
-         
-         nuevtd2.appendChild(nuevIn2);  
-         
-        }
-
-
+        // set datito
+        nuevoTr.innerText = datos[i];
+        // Agregar el nuevo <td> 
+        fil.appendChild(nuevoTr);
+        nuevoTr.appendChild(nuevtd);
+        nuevtd.appendChild(nuevIn);
+        nuevoTr.appendChild(nuevtd2);
+        nuevtd2.appendChild(nuevIn2);
+    }
 }
 
-
-function recuperaDato(){
-    
-     var inicio= document.getElementById('inicio');
-     var final= document.getElementById('final');
-     localStorage.setItem('inicio', inicio.value);
-     localStorage.setItem('fin', final.value);
-
-     var num1 =inicio.value;
+function recuperaDato() {
+    for (var i = 0; i < datos.length; i++) {
+        var inicio = document.getElementById('inicio_' + i);
+        var final = document.getElementById('final_' + i);
+        localStorage.setItem('inicio_' + i, inicio.value);
+        localStorage.setItem('fin_' + i, final.value);
+        var num1 =inicio.value;
      var num2 =final.value;
      var anhioI = parseFloat(num1);
      var anhioF = parseFloat(num2);
      
      var iwei= (((anhioI - anhioF) / anhioI) * 100) * -1;
      console.log("iwei"+ iwei);
-     
-     
     }
+    
+}
+
+
+
+
 
 fill();
 
